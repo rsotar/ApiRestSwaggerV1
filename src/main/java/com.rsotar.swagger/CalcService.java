09761f306ddf93ccd0b9635ae532ec9cba3feca5
Roberto Sotar <rsotar@gmail.com>
2015-06-01 08:23:57 -0500

    Add some funtionalities about Swagger

--- src/main/java/com.rsotar.swagger/CalcService.java
+++ src/main/java/com.rsotar.swagger/CalcService.java
@@ -4,7 +4,11 @@ import com.wordnik.swagger.annotations.Api;
 import com.wordnik.swagger.annotations.ApiOperation;
 import org.springframework.stereotype.Service;
 
-import javax.ws.rs.*;
+import javax.ws.rs.Path;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.GET;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
 
 @Service//spring anotation to maping with the spring context
@@ -12,29 +16,18 @@ import javax.ws.rs.core.MediaType;
 @Api(value = "/calc", description = "Calculator ApiREST Sample")
 public interface CalcService {
 
-    @POST
+    @GET
     @Path("/add/{addNum1}-{addNum2}")
-    @ApiOperation(value = "Operation Add ", response = Factors.class)
+    @ApiOperation(value = "Operation Add ", notes = "Add from two numbers", response = Integer.class)
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     int addOperation (@PathParam("addNum1")int addNum1, @PathParam("addNum2") int addNum2);
 
     @GET
     @Path("/sub/{subNum1}-{subNum2}")
-    @ApiOperation(value = "Sub Operation ", response = Integer.class)
+    @ApiOperation(value = "Operation Sub", notes = "Substraction",response = Integer.class)
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     int subOperation (@PathParam("subNum1")int subNum1, @PathParam("subNum2") int subNum2);
-
-    /*
-    * @POST
-	@Path("/saveUser")
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
-	@ApiOperation(value = "Save user details", response = User.class)
-	public User getUser(User user);
-    * */
-
-
 
 }
